352195ee6fd7fb086ac92a26e5403b077c8ae38f
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-09-19 15:07:17 +0200

    UPSTREAM: USB: fix out-of-bounds in usb_set_configuration
    commit bd7a3fe770ebd8391d1c7d072ff88e9e76d063eb
    
    Andrey Konovalov reported a possible out-of-bounds problem for a USB interface
    association descriptor.  He writes:
    	It seems there's no proper size check of a USB_DT_INTERFACE_ASSOCIATION
    	descriptor. It's only checked that the size is >= 2 in
    	usb_parse_configuration(), so find_iad() might do out-of-bounds access
    	to intf_assoc->bInterfaceCount.
    
    And he's right, we don't check for crazy descriptors of this type very well, so
    resolve this problem.  Yet another issue found by syzkaller...
    
    Bug: 69052055
    Change-Id: I2cc3b5a66d16abd0fc567d69457fc90a45eb12d8
    Reported-by: Andrey Konovalov <andreyknvl@google.com>
    Tested-by: Andrey Konovalov <andreyknvl@google.com>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/config.c
+++ drivers/usb/core/config.c
@@ -515,8 +515,8 @@ static int usb_parse_configuration(struct usb_device *dev, int cfgidx,
 			d = (struct usb_interface_assoc_descriptor *)header;
 			if (d->bLength < USB_DT_INTERFACE_ASSOCIATION_SIZE) {
 				dev_warn(ddev,
-					"config %d has an invalid interface association descriptor of length %d, skipping\n",
-					cfgno, d->bLength);
+					 "config %d has an invalid interface association descriptor of length %d, skipping\n",
+					 cfgno, d->bLength);
 				continue;
 			}
 
