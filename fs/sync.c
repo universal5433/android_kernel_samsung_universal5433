1d84cdeda19e2bf08750521300278bb40570bc33
Faux123 <reioux@gmail.com>
2019-05-31 02:36:06 +0400

    fs: Dynamic Fsync 1.5

--- fs/sync.c
+++ fs/sync.c
@@ -21,6 +21,7 @@
 bool fsync_enabled = true;
 module_param(fsync_enabled, bool, 0755);
 
+
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
 
