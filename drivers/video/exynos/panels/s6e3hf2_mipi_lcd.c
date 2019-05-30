e8141efe036ec0089d2b3bc4ded202e217a28f69
ananjaser1211 <ananjaser@gmail.com>
2019-05-31 02:50:37 +0400

    video: Add powersuspend hooks for tre/tbe

--- drivers/video/exynos/panels/s6e3hf2_mipi_lcd.c
+++ drivers/video/exynos/panels/s6e3hf2_mipi_lcd.c
@@ -25,6 +25,10 @@
 #include <linux/reboot.h>
 #include <linux/of_gpio.h>
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #include <video/mipi_display.h>
 #include "../decon_display/decon_mipi_dsi.h"
 
@@ -2543,6 +2547,10 @@ static int s6e3hf2_displayon(struct mipi_dsim_device *dsim)
 
 	s6e3hf2_power(lcd, FB_BLANK_UNBLANK);
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE); // Yank555.lu : add hook to handle powersuspend tasks (wakeup)
+#endif
+
 	return 0;
 }
 
@@ -2551,6 +2559,10 @@ static int s6e3hf2_suspend(struct mipi_dsim_device *dsim)
 	struct lcd_info *lcd = dev_get_drvdata(&dsim->lcd->dev);
 
 	s6e3hf2_power(lcd, FB_BLANK_POWERDOWN);
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE); // Yank555.lu : add hook to handle powersuspend tasks (sleep)
+#endif
 
 	return 0;
 }
