1cc8cb211ebb2c6bd7d3158ce1129c69f4045fa9
Anan Jaser <ananjaser@gmail.com>
2021-04-20 21:45:05 +0400

    mm: increase max_swappiness
    * and dont depend on ZSWAP

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1178,11 +1178,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
-#ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
 		.extra2		= &max_swappiness,
-#else
-		.extra2		= &one_hundred,
-#endif
 	},
 #ifdef CONFIG_HUGETLB_PAGE
 	{
