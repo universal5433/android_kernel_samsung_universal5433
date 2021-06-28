8f23f5ca3df04d34864c852f3d0564b803997c97
Anan Jaser <ananjaser@gmail.com>
2021-06-28 11:27:15 +0400

    gpu: r15p0: set min_clock to 266MHz
    * 160 is too laggy, and this matches the higher end exynos7420 config
    
    Change-Id: I5b4fba6636a6a5c4805e4a2209e19e2a580f3ef0

--- drivers/gpu/arm/t7xx/r15p0/platform/exynos/gpu_exynos5433.c
+++ drivers/gpu/arm/t7xx/r15p0/platform/exynos/gpu_exynos5433.c
@@ -66,7 +66,7 @@ static int available_max_clock[] = {GPU_L2, GPU_L2, GPU_L0, GPU_L0, GPU_L0};
 static gpu_attribute gpu_config_attributes[] = {
 	{GPU_MAX_CLOCK, 700},
 	{GPU_MAX_CLOCK_LIMIT, 600},
-	{GPU_MIN_CLOCK, 160},
+	{GPU_MIN_CLOCK, 266},
 	{GPU_DVFS_START_CLOCK, 266},
 	{GPU_DVFS_BL_CONFIG_CLOCK, 266},
 	{GPU_GOVERNOR_TYPE, G3D_DVFS_GOVERNOR_INTERACTIVE},
