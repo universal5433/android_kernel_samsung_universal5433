07dabbec8c0b88dbc12793248a450848f59bd4f0
Ian Hope <ian.hope666@gmail.com>
2023-10-29 14:12:42 +1100

    fix compilation warnings about MODPOST
    Change-Id: Id9ab430c20e6d92c01f29118d3e999619e81a3fd

--- drivers/pci/host/pcie-designware.c
+++ drivers/pci/host/pcie-designware.c
@@ -465,7 +465,7 @@ void dw_pcie_config_l1ss(struct pcie_port *pp)
 	}
 }
 
-int dw_pcie_host_init(struct pcie_port *pp)
+int __init dw_pcie_host_init(struct pcie_port *pp)
 {
 	struct device_node *np = pp->dev->of_node;
 	struct of_pci_range range;
