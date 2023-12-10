cc3dccb8d05af5675d76b3ac015d8e2a57def365
Ian Hope <ian.hope666@gmail.com>
2023-12-10 17:18:47 +1100

    fix compilation
    Change-Id: I03f56d7e0d5799a318201e61079184b67eecefa9

--- include/linux/sec_argos.h
+++ include/linux/sec_argos.h
@@ -1,7 +1,6 @@
 #ifndef _SEC_ARGOS_H
 #define _SEC_ARGOS_H
 
-extern int irq_set_affinity(unsigned int irq, const struct cpumask *mask);
 extern void argos_dm_hotplug_enable(void);
 extern void argos_dm_hotplug_disable(void);
 
