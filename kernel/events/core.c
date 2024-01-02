facba32ef0c7a1dba5dcdb383608f31d4007429c
Ian Hope <ian.hope666@gmail.com>
2024-01-02 13:23:59 +1100

    revert
    Change-Id: If5eb73e47bd3e32faf5757d80234ba601b881011

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -3626,6 +3626,19 @@ static long _perf_ioctl(struct perf_event *event, unsigned int cmd, unsigned lon
 	return 0;
 }
 
+static long perf_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+	struct perf_event *event = file->private_data;
+	struct perf_event_context *ctx;
+	long ret;
+
+	ctx = perf_event_ctx_lock(event);
+	ret = _perf_ioctl(event, cmd, arg);
+	perf_event_ctx_unlock(event, ctx);
+
+	return ret;
+}
+
 #ifdef CONFIG_COMPAT
 static long perf_compat_ioctl(struct file *file, unsigned int cmd,
 				unsigned long arg)
